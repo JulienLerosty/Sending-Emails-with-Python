90145fe006b96cf837f818603ae911fe13f4a8ca
JulienLerosty <144648194+JulienLerosty@users.noreply.github.com>
2023-09-21 20:41:57 +0000

    Send email with html

--- email-playground/email_sender.py
+++ email-playground/email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from pathlib import Path
+from string import Template
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Julien Lerosty'
 email['to'] = 'julien.lerosty@gmail.com'
 email['subject'] = 'You won 1M $'
 
-email.set_content('I am Python Master!')
+email.set_content(html.substitute({'name': 'Tintin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
